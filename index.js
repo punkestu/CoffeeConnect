8c391739ee21d35c2335a4503aaa7d7f1bd02f2a
punkestu <pangestubima89@gmail.com>
2023-04-12 13:59:02 +0700

    add morgan logger

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const session = require('express-session');
+const logger = require("morgan");
 
 require('dotenv').config();
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(logger("dev"));
 
 const webRoutes = require("./routes/web");
 const apiRoutes = require("./routes/api");
