2be3c40c34643fa80cf667e2c4695014ccc2ff69
punkestu <pangestubima89@gmail.com>
2023-05-21 23:34:12 +0700

    refactor view and add sidebar + action

--- controller/view/error.js
+++ controller/view/error.js
@@ -1,7 +1,6 @@
 module.exports = {
     notfound: function (req, res) {
         res.render("error/404", {
-            useHeader: true,
             user: req.session.user,
             kedai: req.session.user && req.session.user.Kedai_Profile, title: "Not Found"
         });
