76e021007a271099ca7d847bcbb16f9caf995e76
punkestu <pangestubima89@gmail.com>
2023-05-22 00:29:54 +0700

    refactor file name and add sorting to timeline

--- routes/web/kedai.js
+++ routes/web/kedai.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 const kedaiV = require("../../controller/view/kedai");
 const kedaiC = require("../../controller/logic/kedai");
+const pictureM = require("../../middleware/picture");
 const {
     struct,isAuth
 } = require("../../middleware/auth");
-const produkMid = require("../../middleware/produk");
 
 router.get("/k/:namakedai", kedaiV.kedaiprofile);
-router.post("/k", isAuth, produkMid.save.picture,
+router.post("/k", isAuth, pictureM.save.picture,
     struct.address, struct.phone,
     kedaiC.upsert
 );
